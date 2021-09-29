3439fb75ff10ca7ee97684f3f78a3f701fee21ea
anjali1102 <ac.chauhan1102@gmail.com>
2021-09-29 15:31:14 +0530

    "added functionality"

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
-const dateOfBirth = document.querySelector("#date-pf-birth");
-const luckyNumber = document.querySelector("#lucky-number");
-const checkNumberButton = document.querySelector("#check-number");
+const dateOfBirth=document.querySelector("#date-pf-birth");
+const luckyNumber=document.querySelector("#lucky-number");
+const checkNumberButton= document.querySelector("#check-number");
 
-checkNumberButton.addEventListener("click", function getValues() {
-  console.log(dateOfBirth.value, luckyNumber.value);
-});
+
+function checkNumberIsLucky(){
+  const date=dateOfBirth.value;
+  const sum=calculateSum(dob);
+  console.log(sum);
+}
+
+function calculateSum(date){
+  dob=dob.replaceAll("-", "");
+  let sum=0;
+  for(let index=0; index<dob.length; index++){
+    sum=sum+Number(dob.charAt(index));
+  }
+  return sum;
+}
+
+checkNumberButton.addEventListener("click",checkNumberIsLucky);
