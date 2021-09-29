eec17bc7423b4fd00ddbd66015efda956bf403f1
anjali1102 <ac.chauhan1102@gmail.com>
2021-09-29 15:44:38 +0530

    "updated script file "

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
-const dateOfBirth=document.querySelector("#date-pf-birth");
-const luckyNumber=document.querySelector("#lucky-number");
-const checkNumberButton= document.querySelector("#check-number");
+const dateOfBirth = document.querySelector("#date-pf-birth");
+const luckyNumber = document.querySelector("#lucky-number");
+const checkNumberButton = document.querySelector("#check-number");
 
+function compareValues(sum, luckyNumber) {
+  console.log("#lucky-number");
+}
 
-function checkNumberIsLucky(){
-  const date=dateOfBirth.value;
-  const sum=calculateSum(dob);
+function checkNumberIsLucky() {
+  const dob = dateOfBirth.value;
+  const sum = calculateSum(dob);
   console.log(sum);
 }
 
-function calculateSum(date){
-  dob=dob.replaceAll("-", "");
-  let sum=0;
-  for(let index=0; index<dob.length; index++){
-    sum=sum+Number(dob.charAt(index));
+function calculateSum(dob) {
+  dob = dob.replaceAll("-", "");
+  let sum = 0;
+  for (let index = 0; index < dob.length; index++) {
+    sum = sum + Number(dob.charAt(index));
   }
   return sum;
 }
 
-checkNumberButton.addEventListener("click",checkNumberIsLucky);
+checkNumberButton.addEventListener("click", checkNumberIsLucky);
